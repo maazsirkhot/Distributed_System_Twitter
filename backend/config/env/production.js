13bd12fa00725871b1d0fe07edf9716cbfd8368c
Shubhangi Yadav <shubhangi.yadav@sjsu.edu>
2019-11-09 19:21:47 -0800

    Added rds configuration

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -6,9 +6,9 @@ module.exports = {
 	database: {
 		mongoDbUrl: process.env.MONGODB_URL || 'mongodb+srv://admin:admin@cluster0-fgxv7.mongodb.net/twitter?retryWrites=true&w=majority',
 		name: process.env.DATABASE || 'twitter',
-		user: process.env.DB_USERNAME || 'put-rds-userid-here',
-		password: process.env.DB_PASSWORD || 'put-rds-password-here',
-		host: process.env.DB_HOST || 'put-rds-host-here',
+		user: process.env.DB_USERNAME || 'root',
+		password: process.env.DB_PASSWORD || '12345678',
+		host: process.env.DB_HOST || 'grubhub.cwotr7vrym6h.us-west-1.rds.amazonaws.com',
 		port: process.env.DB_DBPORT || '3306',
 		dialect: process.env.DB_DIALECT || 'mysql'
 	}
