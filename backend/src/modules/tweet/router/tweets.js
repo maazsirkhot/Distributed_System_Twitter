b7d1c75c3221a1d4846f3697c2f342889689a8a6
farhakauser-1 <farha.kauser1@gmail.com>
2019-11-12 05:58:22 -0800

    Adding fetch Tweet by tweet ID

--- backend/src/modules/tweet/router/tweets.js
+++ backend/src/modules/tweet/router/tweets.js
@@ -1,15 +1,36 @@
-`use strict`
-
 import express from 'express'
-let router = express.Router()
 import tweetController from '../controller/tweets'
 import validator from '../validator'
 import validation from 'express-validation'
-require('../../../middlewares/passport')
 import passport from 'passport'
 
-router.post('/createTweet', validation(validator['createTweet']), passport.authenticate('jwt', { session: false }), tweetController.createTweet)
-router.post('/addComment', validation(validator['addComment']), passport.authenticate('jwt', { session: false }), tweetController.addComment)
-router.delete('/:tweetId', validation(validator['deleteTweet']), passport.authenticate('jwt', { session: false }), tweetController.deleteTweet)
+  ;`use strict`
+let router = express.Router()
+require('../../../middlewares/passport')
+
+router.post(
+  '/createTweet',
+  validation(validator['createTweet']),
+  passport.authenticate('jwt', { session: false }),
+  tweetController.createTweet
+)
+router.post(
+  '/addComment',
+  validation(validator['addComment']),
+  passport.authenticate('jwt', { session: false }),
+  tweetController.addComment
+)
+router.delete(
+  '/:tweetId',
+  validation(validator['deleteTweet']),
+  passport.authenticate('jwt', { session: false }),
+  tweetController.deleteTweet
+)
+router.get(
+  '/:tweetId',
+  validation(validator['fetchTweetbyID']),
+  passport.authenticate('jwt', { session: false }),
+  tweetController.fetchTweetbyID
+)
 
 module.exports = router
--- backend/src/modules/tweet/validator.js
+++ backend/src/modules/tweet/validator.js
@@ -1,40 +1,54 @@
-`use strict`
-
 import Joi from 'joi'
 
+  ;`use strict`
+
 module.exports = {
-    createTweet: {
-        body: {
-            userId: Joi.string().required(),
-            userName: Joi.string().required(),
-            userImageURL: Joi.string().required(),
-            originalBody: Joi.string().max(280),
-            imageURL: Joi.string(),
-            tweetId: Joi.string(),
-            isRetweet: Joi.boolean(),
-        },
-        model: "createTweet",
-        group: "Tweet",
-        description: "Create tweet and save details in database"
+  createTweet: {
+    body: {
+      userId: Joi.string().required(),
+      userName: Joi.string().required(),
+      userImageURL: Joi.string().required(),
+      originalBody: Joi.string().max(280),
+      imageURL: Joi.string(),
+      tweetId: Joi.string(),
+      isRetweet: Joi.boolean()
     },
-    addComment: {
-        body: {
-            tweetId : Joi.string().required(),
-            userId : Joi.string().required(),
-            userName : Joi.string().required(),
-            imageURL : Joi.string().required(),
-            body : Joi.string().max(280).required(),
-        },
-        model: "addComment",
-        group: "Tweet",
-        description: "Add a comment on tweet and its retweet and save details in database"
+    model: 'createTweet',
+    group: 'Tweet',
+    description: 'Create tweet and save details in database'
+  },
+  addComment: {
+    body: {
+      tweetId: Joi.string().required(),
+      userId: Joi.string().required(),
+      userName: Joi.string().required(),
+      imageURL: Joi.string().required(),
+      body: Joi.string()
+        .max(280)
+        .required()
     },
-    deleteTweet: {
-        body: {
-            tweetId : Joi.string().required(),
-        },
-        model: "deleteTweet",
-        group: "Tweet",
-        description: "Mark tweet as deleted"
+    model: 'addComment',
+    group: 'Tweet',
+    description:
+      'Add a comment on tweet and its retweet and save details in database'
+  },
+  deleteTweet: {
+    body: {
+      tweetId: Joi.string().required()
     },
+    model: 'deleteTweet',
+    group: 'Tweet',
+    description: 'Mark tweet as deleted'
+  },
+  fetchTweetbyID: {
+    path: {
+      tweetId: Joi.string().required()
+    },
+    header: {
+      authorization: Joi.string().required()
+    },
+    model: 'fetchTweetbyID',
+    group: 'Search',
+    description: 'Tweet ID is supplied and the tweet is returned'
+  }
 }
