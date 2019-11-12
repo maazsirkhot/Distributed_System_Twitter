69fcea4dd2018c5ec965671d7c378985c833cffd
Maaz Sirkhot <maazsirkhot0@gmail.com>
2019-11-11 22:07:31 -0800

    Commented config data and added passport to Message routes

--- backend/src/modules/messages/router/messages.js
+++ backend/src/modules/messages/router/messages.js
@@ -8,8 +8,8 @@ import validation from 'express-validation'
 require('../../../middlewares/passport');
 import passport from 'passport'
 
-router.post('/send', validation(validator['sendMessage']), messageController.sendMessage);
-router.get('/inbox', validation(validator['getInbox']), messageController.getInbox);
-router.get('/conversation', validation(validator['getConversation']), messageController.getConversation);
+router.post('/send', validation(validator['sendMessage']), passport.authenticate('jwt', { session: false }), messageController.sendMessage);
+router.get('/inbox', validation(validator['getInbox']), passport.authenticate('jwt', { session: false }), messageController.getInbox);
+router.get('/conversation', validation(validator['getConversation']), passport.authenticate('jwt', { session: false }), messageController.getConversation);
 
 module.exports = router
