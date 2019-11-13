40088174b0f38342ada9eebc8931fdb30a2df71d
Aswin <aswin-pc@MacBook-Pro.local>
2019-11-12 17:09:19 -0800

    Tweet - routes, removed duplicates

--- backend/src/modules/tweet/router/tweets.js
+++ backend/src/modules/tweet/router/tweets.js
@@ -34,27 +34,6 @@ router.get(
   passport.authenticate('jwt', { session: false }),
   tweetController.fetchTweetById
 )
-
-require('../../../middlewares/passport')
-
-router.post(
-  '/createTweet',
-  validation(validator['createTweet']),
-  passport.authenticate('jwt', { session: false }),
-  tweetController.createTweet
-)
-router.post(
-  '/addComment',
-  validation(validator['addComment']),
-  passport.authenticate('jwt', { session: false }),
-  tweetController.addComment
-)
-router.delete(
-  '/:tweetId',
-  validation(validator['deleteTweet']),
-  passport.authenticate('jwt', { session: false }),
-  tweetController.deleteTweet
-)
 router.get(
   '/fetchTweetByUserID/',
   validation(validator['getTweets']),
