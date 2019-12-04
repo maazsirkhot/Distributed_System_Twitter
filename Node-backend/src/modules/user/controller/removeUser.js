af5fec1f53bb1413d168c87b61dabc9843c58cd5
jayasurya17 <jayasurya1796@gmail.com>
2019-12-03 18:17:41 -0800

    Migrated messages and tweets to kafka

--- Node-backend/src/modules/user/controller/removeUser.js
+++ Node-backend/src/modules/user/controller/removeUser.js
@@ -1,127 +1,31 @@
 'use strict'
 
-import Users from '../../../models/mongoDB/users'
-import Tweets from '../../../models/mongoDB/tweets'
-import Messages from '../../../models/mongoDB/messages'
-import Lists from '../../../models/mongoDB/lists'
-import constants from '../../../utils/constants'
-import mongoose from 'mongoose'
-import model from '../../../models/sqlDB/index'
-
-let Sequelize = require('sequelize');
-var Op = Sequelize.Op;
-
 /**
  * Create user and save data in database.
  * @param  {Object} req request object
  * @param  {Object} res response object
  */
-exports.deleteUser = async (req, res) => {
-    var userDetails = {
-        userId : req.body.userId,
-        userName : req.body.userName
-    }
-    try {
-        var checkUser = await Users.findById(userDetails.userId);
-        console.log(checkUser);
-        //const Op = Sequelize.Op;
-        if(checkUser){
+exports.deleteUser = async (r, res) => {
 
-            //Get Tweets by the user
-            let allTweets = await Tweets.find({userId : mongoose.Types.ObjectId(checkUser._id)})
-            console.log(allTweets);
-            var tweetid = []
-            allTweets.forEach(tweet => {
-                tweetid.push(String(tweet._id));
-            })
+	console.log('--------------', r.route.path, '-----------------');
 
-            console.log(tweetid);
+	let req = {};
+	req.body = r.body;
+	req.path = r.route.path;
 
-            //Delete likes on other tweets
-            var deleteLikesOnTweets = await model.likes.destroy({
-                where : {tweetId : {[Op.in] : tweetid}}
-            }) 
-            console.log(deleteLikesOnTweets);
-
-            //Delete User Profile
-            var deleteProfile = await Users.deleteOne({_id : mongoose.Types.ObjectId(checkUser._id)});
-            console.log(deleteProfile);
-
-            //Delete Tweets by User
-            var deleteTweets = await Tweets.deleteMany({ $or : [{userId : mongoose.Types.ObjectId(checkUser._id)}, {originalUserId : mongoose.Types.ObjectId(checkUser._id)}]});
-            console.log(deleteTweets);
-
-            //Delete Message conversation by User
-            var deleteMessages = await Messages.deleteMany({ participants : {$elemMatch : {userId : mongoose.Types.ObjectId(checkUser._id)}}})
-            console.log(deleteMessages);
-
-            //Delete comments on other's tweets
-            var deleteComments = Tweets.updateMany({ comments : { $elemMatch : {userId : mongoose.Types.ObjectId(checkUser._id)}}}, { $pull : {comments : { userId : mongoose.Types.ObjectId(checkUser._id)}}}, {multi : true});
-            console.log(deleteComments);
-
-            //Delete Lists created by User
-            var deleteLists = await Lists.deleteMany({ownerId : mongoose.Types.ObjectId(checkUser._id)});
-            console.log(deleteLists);
-
-            //Delete from other Lists and decrease member count
-            var deleteFromLists = await Lists.updateMany({membersId : { $elemMatch : {memberId : mongoose.Types.ObjectId(checkUser._id)}}}, { $pull : {membersId : {memberId : mongoose.Types.ObjectId(checkUser._id)}}, "$inc" : { noOfMembers : -1}}, {multi : true});
-            console.log(deleteFromLists);
-
-            //SQL compatible id
-            var id = String(checkUser._id);
-            //console.log(id)
-
-            //Delete from other's follower lists and own followers
-            var deleteFollow = await model.follows.destroy({
-                where : {
-                    [Op.or] : [
-                        {userId : id},
-                        {followerId : id}
-                    ]
-                }
-            })
-            console.log(deleteFollow);
-
-            //Delete Likes on tweets
-            var deleteLikes = await model.likes.destroy({
-                where : {userId : id}
-            })
-            console.log(deleteLikes);
-
-            //Delete from subscriber list
-            //Get all list Ids subscribed to
-            var listIds = await model.listSubscribers.findAndCountAll({
-                where : {
-                    subscriberId : id
-                }
-            })
-            var listIdArray = [];
-            listIds.rows.forEach(row => {
-                listIdArray.push(mongoose.Types.ObjectId(row.listId));
-            })
-            console.log(listIdArray);
-            
-            //Delete from subscriber list
-            var deleteListSubscribers = await model.listSubscribers.destroy({
-                where : {subscriberId : id}
-            })
-
-            //Decrement subscriber count for respective lists
-            if(listIdArray.length > 0){
-                var decSubscribers = await Lists.updateMany({_id : { $in : listIdArray}}, { "$inc" : { noOfSubscribers : -1}}, {multi : true});
-            }
-            console.log(deleteListSubscribers);
-            
-            res.status(constants.STATUS_CODE.SUCCESS_STATUS).send("Delete User Activity Completed");
-        } else {
-            res.status(constants.STATUS_CODE.NOT_FOUND_STATUS).send(checkUser)
+	kafka.make_request('users', req, function(err, results){
+        console.log(results);
+        if (err){
+            console.log("Inside err");
+            res.json({
+                status:"error",
+                msg:"System Error, Try Again."
+            });
+        }else{
+            console.log("Inside else");
+			return res
+				.status(results.status)
+				.send(results.message)
         }
-    }
-    catch(error){
-        console.log(`Error while creating user ${error}`)
-		return res
-			.status(constants.STATUS_CODE.INTERNAL_SERVER_ERROR_STATUS)
-			.send(error)
-    }
-
+    });
 }
---
+++ kafka-backend/services/tweets.js
@@ -0,0 +1,45 @@
+import TweetContoller from '../src/modules/tweet/controller/tweets'
+
+async function handle_request(req, callback){
+   
+    console.log("Inside User kafka backend");
+
+    console.log('------------', req.path, '----------------');
+
+    let results;
+    switch(req.path) {
+      case '/createTweet': results = await TweetContoller.createTweet(req);
+      break;
+      case '/addComment': results = await TweetContoller.addComment(req);
+      break;
+      case '/:tweetId': results = await TweetContoller.deleteTweet(req);
+      break;
+      case '/fetchTweetById/:tweetId': results = await TweetContoller.fetchTweetById(req);
+      break;
+      case '/fetchTweetByUserID/:userId/:taskName': results = await TweetContoller.getTweets(req);
+      break;
+      case '/topTweetsByLike/:userId': results = await TweetContoller.topTweetsByLike(req);
+      break;
+      case '/topTweetsByRetweets/:userId': results = await TweetContoller.topTweetsByRetweets(req);
+      break;
+      case '/topTweetsByViews/:userId': results = await TweetContoller.topTweetsByViews(req);
+      break;
+      case '/tweetsByMonth/:userId': results = await TweetContoller.tweetsByMonth(req);
+      break;
+      case '/tweetsByDay/:userId/:month/:year': results = await TweetContoller.tweetsByDay(req);
+      break;
+      case '/tweetsByHour/:userId/:day/:month/:year': results = await TweetContoller.tweetsByHour(req);
+      break;
+      case '/likeTweet': results = await TweetContoller.likeTweet(req);
+      break;
+      case '/fetchTweetForList/:listId': results = await TweetContoller.getTweetsForList(req);
+      break;
+      case '/searchByHashTag': results = await TweetContoller.searchByHashTag(req);
+      break;
+    }
+
+    callback(null, results);
+    console.log("after callback");
+};
+
+exports.handle_request = handle_request;
